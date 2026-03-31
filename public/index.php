c68d20051663aaa95e92a348ef5aab6a0caaf9b1
Etufrank <frankrazni@gmail.com>
2026-03-31 12:04:49 +0300

    changement

--- public/index.php
+++ public/index.php
@@ -1,14 +1,10 @@
 <?php
-// Point d'entrée minimal pour l'application (sans CSS)
-// Démarre Flight, charge la DB, les models et controllers, et définit les routes principales.
 
 require_once __DIR__ . '/../vendor/autoload.php';
 session_start();
 
-// Charger la connexion PDO
 $pdo = require __DIR__ . '/../app/config/database.php';
 
-// Charger models et controllers (fichiers du projet)
 foreach (glob(__DIR__ . '/../app/models/*.php') as $file) {
 	require_once $file;
 }
@@ -16,10 +12,8 @@ foreach (glob(__DIR__ . '/../app/controllers/*.php') as $file) {
 	require_once $file;
 }
 
-// Configurer le chemin des vues pour Flight
 Flight::set('flight.views.path', __DIR__ . '/../app/views');
 
-// Routes principales
 Flight::route('/', function() {
 	Flight::redirect('/choix-caisse');
 });
@@ -64,5 +58,29 @@ Flight::route('/produit/@id', function($id) use ($pdo) {
 	$ctrl->details($id);
 });
 
-// Démarrer Flight
+Flight::route('/choix_caisse', function() use ($pdo) {
+	$ctrl = new CaisseController($pdo);
+	$ctrl->afficherChoix();
+});
+
+Flight::route('POST /select_caisse', function() use ($pdo) {
+	$ctrl = new CaisseController($pdo);
+	$ctrl->selectionner();
+});
+
+Flight::route('/saisie_achat', function() use ($pdo) {
+	$ctrl = new AchatController($pdo);
+	$ctrl->afficherSaisie();
+});
+
+Flight::route('POST /add_product', function() use ($pdo) {
+	$ctrl = new AchatController($pdo);
+	$ctrl->ajouterLigne();
+});
+
+Flight::route('POST /cloturer_achat', function() use ($pdo) {
+	$ctrl = new AchatController($pdo);
+	$ctrl->validerAchat();
+});
+
 Flight::start();
